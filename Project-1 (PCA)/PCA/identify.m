b8d034ff216fd5569b3f7acdd46ab31334eece54
bereketeshete <40177156+bereketeshete@users.noreply.github.com>
2021-05-09 16:14:59 +0900

    Add files via upload

---
+++ Project-1 (PCA)/PCA/identify.m
@@ -0,0 +1,10 @@
+function [recognized_img] = identify(projectimg,projtestimg)
+
+%% Arguments %%
+% project_train_img : training matrix represented by k PCs
+% project_test_img : test matrix represented by k PCs
+
+%% Your code here %%
+% Hint: Use Euclidean distance to measure similarity.
+
+end
